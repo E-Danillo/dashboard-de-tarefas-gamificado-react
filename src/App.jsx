db40a0e71075f17e0a546ac3c10f98c41f9805a2
Emerson Danillo <danilloemerson0@gmail.com>
2026-03-28 16:37:52 -0300

    style: CSS completo

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import { useState } from "react"
+import './App.css'
+
 
 function App() {
 
@@ -71,50 +73,46 @@ function App() {
   }
 
 return (
-  <div>
+  <div id="div-principal">
     <h1>Dashboard</h1>
     <p>Nível: {calcularNivel(xp).nivel}</p>
     <p>XP: {xp}</p>
-    <div style={{ width: "100%", background: "#333", borderRadius: "8px", height: "12px" }}>
-    <div style={{ 
-      width: calcularPorcentagem() + "%",
-      background: "green", 
-      height: "12px",
-      borderRadius: "8px"
-    }}>
-    </div>
+
+    <div id="barra_nivel">
+      <div id="barra_xp" style={{ width: calcularPorcentagem() + "%" }}></div>
     </div>
 
-    <button onClick={() => setFormularioAberto(true)}>Nova Tarefa</button>
+    <button id="criar_nova_tarefa" onClick={() => setFormularioAberto(true)}>Nova Tarefa</button>
 
-    {/* se formularioAberto for true, mostra o div. Se for false, não mostra nada. */}
-    {formularioAberto && (
-      <div>
-        <input
-          type="text"
-          value={inputTexto}
-          onChange={(e) => setInputTexto(e.target.value)}
-          placeholder="Nome da tarefa"
-        />
-        <select value={prioridade} onChange={(e) => setPrioridade(e.target.value)}>
-          <option>Alta</option>
-          <option>Média</option>
-          <option>Baixa</option>
-        </select>
-        <button onClick={adicionarTarefa}>Adicionar</button>
-        <button onClick={() => setFormularioAberto(false)}>Cancelar</button>
+    <div className={`div_do_formulario ${formularioAberto ? "ativada" : ""}`}>
+      <input
+        id="input"
+        type="text"
+        value={inputTexto}
+        onChange={(e) => setInputTexto(e.target.value)}
+        placeholder="Nome da tarefa"
+      />
+      <select value={prioridade} onChange={(e) => setPrioridade(e.target.value)}>
+        <option>Alta</option>
+        <option>Média</option>
+        <option>Baixa</option>
+      </select>
+      <div id="botoes-formulario">
+        <button id="adicionar" onClick={adicionarTarefa}>Adicionar</button>
+        <button id="cancelar" onClick={() => setFormularioAberto(false)}>Cancelar</button>
       </div>
-    )}
+    </div>
 
-    {/* "para cada `tarefa` do array listaDeTarefas, cria um `<li>` com o texto e a prioridade dela." */}
-    <ul> 
-        {listaDeTarefas.map(tarefa => (
-          <li key = {tarefa.id}>
-            {<span style={{ textDecoration: tarefa.concluida ? "line-through" : "none", opacity: tarefa.concluida ? "0.6" : "1" }}>{tarefa.texto}</span>} - {tarefa.prioridade}
-            <button onClick={() => excluirTarefa(tarefa.id)}>❌</button>
-            <button onClick={() => concluirTarefa(tarefa.id)}>✅</button>
-          </li>
-        ))}
+    <ul id="lista_de_tarefas">
+      {listaDeTarefas.map(tarefa => (
+        <li key={tarefa.id}>
+          <span style={{ textDecoration: tarefa.concluida ? "line-through" : "none", opacity: tarefa.concluida ? "0.6" : "1" }}>
+            {tarefa.prioridade === "Alta" ? "🔴" : tarefa.prioridade === "Média" ? "🟠" : "🟡"} {tarefa.texto}
+          </span>
+          <button onClick={() => excluirTarefa(tarefa.id)}>❌</button>
+          <button onClick={() => concluirTarefa(tarefa.id)}>✅</button>
+        </li>
+      ))}
     </ul>
   </div>
 )
