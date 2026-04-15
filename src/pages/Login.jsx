0636a06fbd5b8aba47f1dd2710def97939d58ff3
Emerson Danillo <danilloemerson0@gmail.com>
2026-04-15 17:56:36 -0300

    style: adição de icon na tela de login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,16 +1,20 @@
 // Login.jsx
 import styles from './Login.module.css'
+import icon from './IconDashboardGamificado.png'
 
 export function Login({ onLogin }) {
   return (
-    <div className={styles.container}>
-      <div className={styles.box}>
-        <h1>Login</h1>
-        <h3 className={styles.label}>Email</h3>
-        <input placeholder="seu@email.com" className={styles.input} />
-        <h3 className={styles.label}>Senha</h3>
-        <input type="password" placeholder="******" className={styles.input}/>
-        <button className={styles.button} onClick={onLogin}>Entrar</button>
+    <div>
+      <div className={styles.container}>
+        <div className={styles.box}>
+        <img className={styles.icon} src={icon} alt="Logo do Dashboard Gamificado"/>
+          <h1>Login</h1>
+          <h3 className={styles.label}>Email</h3>
+          <input placeholder="seu@email.com" className={styles.input} />
+          <h3 className={styles.label}>Senha</h3>
+          <input type="password" placeholder="******" className={styles.input}/>
+          <button className={styles.button} onClick={onLogin}>Entrar</button>
+        </div>
       </div>
     </div>
   )
