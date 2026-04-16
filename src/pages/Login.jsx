559a382579624d337897b5da7ea93775066c8d6d
Emerson Danillo <danilloemerson0@gmail.com>
2026-04-16 09:16:27 -0300

    feat: adição do slogan e copyright no Login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -4,17 +4,22 @@ import icon from './IconDashboardGamificado.png'
 
 export function Login({ onLogin }) {
   return (
-    <div>
+    <div className={styles.containerPrincipal}>
       <div className={styles.container}>
-        <div className={styles.box}>
-        <img className={styles.icon} src={icon} alt="Logo do Dashboard Gamificado"/>
-          <h1>Login</h1>
-          <h3 className={styles.label}>Email</h3>
-          <input placeholder="seu@email.com" className={styles.input} />
-          <h3 className={styles.label}>Senha</h3>
-          <input type="password" placeholder="******" className={styles.input}/>
-          <button className={styles.button} onClick={onLogin}>Entrar</button>
-        </div>
+        <h1 className={styles.nomeDoSite}>KillTheDay</h1>
+        <p className={styles.fraseImpacto}>O dia te testa. Você responde vencendo.</p>
+          <div className={styles.box}>
+            <img className={styles.icon} src={icon} alt="Logo do Dashboard Gamificado"/>
+            <h2>Login</h2>
+            <h3 className={styles.label}>Email</h3>
+            <input placeholder="seu@email.com" className={styles.input} />
+            <h3 className={styles.label}>Senha</h3>
+            <input type="password" placeholder="******" className={styles.input}/>
+            <button className={styles.button} onClick={onLogin}>Entrar</button>
+          </div>
+        <footer className={styles.footer}>
+          © 2026 Emerson Danillo. Todos os direitos reservados.
+        </footer>
       </div>
     </div>
   )
